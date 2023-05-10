3dbfe57e75fbc7c7c27ace32501ab9b5746eff1e
hoonglily <hoonglily4@gmail.com>
2023-05-09 22:02:04 -0700

    finished one button for dark/light more

--- client/App.tsx
+++ client/App.tsx
@@ -1,9 +1,90 @@
-import React, { useState } from "react";
-import IconButton from "@mui/material/IconButton";
-import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
-import Brightness4Icon from "@mui/icons-material/Brightness4";
-import Brightness7Icon from "@mui/icons-material/Brightness7";
+// import React, { useState } from "react";
+// import IconButton from "@mui/material/IconButton";
+// import { useTheme, ThemeProvider, createTheme } from "@mui/material/styles";
+// import Brightness4Icon from "@mui/icons-material/Brightness4";
+// import Brightness7Icon from "@mui/icons-material/Brightness7";
 
+// import "./App.Styles.scss";
+// import "./components/TreeDiagram/TreeDiagramStyles.scss";
+// import "./components/DisplayGraphiql/DisplayGraphiql.scss";
+
+// import Navbar from './components/NavBar/navbar';
+// import Visualizer from "./components/Visualizer/Visualizer";
+// import DisplayGraphiql from "./components/DisplayGraphiql/DisplayGraphiql";
+// import { PortContextProvider } from "./contextStore/port-context";
+
+// const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+
+// function MyApp() {
+//   const theme = useTheme();
+//   const colorMode = React.useContext(ColorModeContext);
+//   return (
+//     <div className="toggle-container">
+//       <IconButton
+//         sx={{ ml: 1 }}
+//         onClick={colorMode.toggleColorMode}
+//         color="inherit"
+//       >
+//         {theme.palette.mode === "dark" ? (
+//           <Brightness7Icon />
+//         ) : (
+//           <Brightness4Icon />
+//         )}
+//       </IconButton>
+//     </div>
+//   );
+// }
+
+// export default function App() {
+
+//   const [mode, setMode] = React.useState<"light" | "dark">(
+//     () => (localStorage.getItem("color-mode") as "light" | "dark") || "light"
+//   );
+
+//   const colorMode = React.useMemo(
+//     () => ({
+//       mode,
+//       toggleColorMode: () => {
+//         const newMode = mode === "light" ? "dark" : "light";
+//         setMode(newMode);
+//         localStorage.setItem("color-mode", newMode);
+//       },
+//     }),
+//     [mode]
+//   );
+
+//   const theme = React.useMemo(
+//     () =>
+//       createTheme({
+//         palette: {
+//           mode,
+//         },
+//       }),
+//     [mode]
+//   );
+
+//   React.useEffect(() => {
+//     document.body.style.backgroundColor =
+//       mode === "dark" ? "hsl(219, 29%, 18%)" : "hsl(219, 28%, 100%)";
+//   }, [mode]);
+
+//   return (
+//     <ColorModeContext.Provider value={colorMode}>
+//       <ThemeProvider theme={theme}>
+//         <PortContextProvider>
+//           <Navbar />
+//           <MyApp />
+//           <div className="mainDiv">
+//             <DisplayGraphiql />
+//             <Visualizer />
+//           </div>
+//         </PortContextProvider>
+//       </ThemeProvider>
+//     </ColorModeContext.Provider>
+//   );
+// }
+
+import React, { useEffect } from "react";
 import "./App.Styles.scss";
 import "./components/TreeDiagram/TreeDiagramStyles.scss";
 import "./components/DisplayGraphiql/DisplayGraphiql.scss";
@@ -13,73 +94,42 @@ import Visualizer from "./components/Visualizer/Visualizer";
 import DisplayGraphiql from "./components/DisplayGraphiql/DisplayGraphiql";
 import { PortContextProvider } from "./contextStore/port-context";
 
-const ColorModeContext = React.createContext({ toggleColorMode: () => {} });
+function App() {
+  useEffect(() => {
+    const body = document.body;
 
-function MyApp() {
-  const theme = useTheme();
-  const colorMode = React.useContext(ColorModeContext);
+    const observer = new MutationObserver((mutationsList) => {
+      for (const mutation of mutationsList) {
+        if (mutation.attributeName === "class") {
+          const isDarkMode = body.classList.contains("graphiql-dark");
+          if (isDarkMode) {
+            document.documentElement.style.setProperty("--app-background-color", "hsl(219, 29%, 18%)");
+          } else {
+            document.documentElement.style.setProperty("--app-background-color", "hsl(219, 28%, 100%)");
+          }
+        }
+      }
+    });
+
+    observer.observe(body, { attributes: true });
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   return (
-    <div className="toggle-container">
-      <IconButton
-        sx={{ ml: 1 }}
-        onClick={colorMode.toggleColorMode}
-        color="inherit"
-      >
-        {theme.palette.mode === "dark" ? (
-          <Brightness7Icon />
-        ) : (
-          <Brightness4Icon />
-        )}
-      </IconButton>
+    <div className="app">
+      <PortContextProvider>
+           <Navbar />
+           <div className="mainDiv">
+             <DisplayGraphiql />
+             <Visualizer />
+           </div>
+         </PortContextProvider>
     </div>
   );
 }
 
-export default function App() {
+export default App;
 
-  const [mode, setMode] = React.useState<"light" | "dark">(
-    () => (localStorage.getItem("color-mode") as "light" | "dark") || "light"
-  );
-
-  const colorMode = React.useMemo(
-    () => ({
-      mode,
-      toggleColorMode: () => {
-        const newMode = mode === "light" ? "dark" : "light";
-        setMode(newMode);
-        localStorage.setItem("color-mode", newMode);
-      },
-    }),
-    [mode]
-  );
-
-  const theme = React.useMemo(
-    () =>
-      createTheme({
-        palette: {
-          mode,
-        },
-      }),
-    [mode]
-  );
-
-  React.useEffect(() => {
-    document.body.style.backgroundColor =
-      mode === "dark" ? "hsl(219, 29%, 18%)" : "hsl(219, 28%, 100%)";
-  }, [mode]);
-
-  return (
-    <ColorModeContext.Provider value={colorMode}>
-      <ThemeProvider theme={theme}>
-        <PortContextProvider>
-          <Navbar />
-          <MyApp />
-          <div className="mainDiv">
-            <DisplayGraphiql />
-            <Visualizer />
-          </div>
-        </PortContextProvider>
-      </ThemeProvider>
-    </ColorModeContext.Provider>
-  );
-}
